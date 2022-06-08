01dd3cdfa443039ef1064008c816055df81ebe23
unknown <jfhiggins98@gmail.com>
2022-06-08 12:24:10 -0400

    Frontend Fundamentals Complete!

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+
+    return <h1>Basic React frontend fr Connect-Lite!</h1>;
+
+ }
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
