ef002ae4282d2a6e6eaf990bf672f57f2e4824dd
John Higgins <jhiggs@Johns-MacBook-Pro.local>
2022-06-11 14:49:34 -0400

    Added protected pages and a Landing Page

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import { Home } from '../screens/Home/Home';
 import { Skills } from '../screens/Skills/Skills';
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { Landing } from '../screens/Landing/Landing';
 
 function getItem(label, key, icon, children, type) {
     return {
@@ -25,7 +26,7 @@ const CustomSider = () => {
     const { isAuthenticated } = useAuth0()
 
     const siderItems = isAuthenticated ? [
-            getItem("Home", '/', <HomeOutlined />),
+            getItem("Home", '/home', <HomeOutlined />),
             getItem("My Skills", '/skills', <UserOutlined />),
         ]
         :
@@ -33,13 +34,16 @@ const CustomSider = () => {
 
     return (
         <Sider >
-            <Menu onClick={(event) =>{navigate(event.key)}}   theme="dark" defaultSelectedKeys={"/"} mode="inline" items={siderItems} />
+            <Menu onClick={(event) =>{navigate(event.key)}} theme="dark" defaultSelectedKeys={location == "/home" ? location : "" } mode="inline" items={siderItems} />
         </Sider>
     )
 }
 
+
+
 const CustomHeader = () => {
     const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0()
+    const navigate = useNavigate()
 
     const authItem = isAuthenticated ? 
         [ getItem("Sign Out", '1', <LogoutOutlined />)]
@@ -54,6 +58,14 @@ const CustomHeader = () => {
             <Menu onClick={() =>{ isAuthenticated ? logout() : loginWithRedirect() }} theme="dark" mode="inline" items={authItem} />
         </Header>
     )
+}
+
+const RequireAuth = ({ children }) => {
+    const { isAuthenticated } = useAuth0()
+    const navigate = useNavigate()
+    
+    if (!isAuthenticated) navigate("/")
+    return children;
 }
 
 
@@ -71,15 +83,23 @@ export const App = () => {
 
                     <Layout style={{"minHeight": "100vh"}}>
                         <CustomSider/>
+                        <Layout style={{justifyContent : "space-between", height : "100vh"}}>
+                            <div style={{ backgroundColor : "white", height: "100vh", padding : "24px 0 0 0"}}>
+                                <Routes>
+                                    <Route path="/" element={<Landing />} />
+                                    <Route path="/home" element={<RequireAuth> <Home/> </RequireAuth>  }/>
+                                    <Route path="/skills" element={<RequireAuth> <Skills /> </RequireAuth>} />
+                                </Routes>
+                            </div>
 
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/skills" element={<Skills />} />
-                        </Routes>
 
-                        <Footer>
-                            Footer
-                        </Footer>
+                            <Footer>
+                                <h4>Forward Motion Program Summer 2022</h4>
+                                <h5>Created by John Higgins at Jahnel Group</h5>
+                            </Footer>
+                        </Layout>
+
+
                     </Layout>
                 </Layout>
 
---
+++ client/src/screens/Landing/Landing.js
@@ -0,0 +1,13 @@
+import 'antd/dist/antd.css';
+import './Landing.css';
+import React from 'react';
+
+export const Landing = () => {
+    return (
+        <div>
+            <h1> Welcome to Connect-Lite </h1>
+            <p> This app was created to learn the GRAND stack.</p>
+        </div>
+        
+    )
+}
